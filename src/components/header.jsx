cfa97d7725430daa9cd5d027c115d7b688b70f4c
Tebibux <tebibusolomon79@gmail.com>
2025-10-17 06:56:11 +0300

    started shop

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <div>
+            <div className="logo">logo here</div>
+            <div className="nav">chart</div>
+            <div className="user">user</div>
+        </div>
+    )
+}
+
+export default header   
