139a7a5596ec9000c855fd4cc75520f2519c6ab4
Tebibux <tebibusolomon79@gmail.com>
2025-11-24 10:14:45 +0300

    some changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,14 @@
+
 const Header = () => {
     return (
-        <div>
-            <div className="text-3x1 font-bold underline">Warka Printing Shop</div>
-            <div className="nav">chart</div>
-            <div className="user">user</div>
+        <div className="bg-gray-900 text-white">
+            <nav aria-label="Global" className="mx-auto flex max-w-7x1 items-center justify-between p-6 lg:px-8">
+                <div className="text-3x1 font-bold underline">Warka Printing Shop</div>
+                <div className="flex items-center justify-between">
+                    <div className="nav px-8">Cart</div>
+                    <div className="user px-8">User</div>
+                </div>
+            </nav>
         </div>
     )
 }
